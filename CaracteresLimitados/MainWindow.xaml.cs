8a9075803b88165f15d5feed83ba75df9792cd7c
JAcutillas <ja.cutillas@iesdoctorbalmis.com>
2021-09-29 11:51:18 +0200

    Agregar archivos de proyecto.

---
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextoActualizadoPrincipalTextBox(Object sender, TextChangedEventArgs args)
+
+        {
+            int numeroCaracteres = PrincipalTextBox.Text.ToCharArray().Length;
+
+            if (numeroCaracteres >= 140) 
+            {
+                PrincipalTextBox.IsReadOnly = true;
+                DarEnhorabuena();
+            }
+            
+            ContadorTextBlock.Text = numeroCaracteres + "/140";
+        }
+
+        private void DarEnhorabuena()
+        {
+            MensajeTextBlock.Text = "";
+        }
+    }
+}
